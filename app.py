3954e5c4203dd55ac73d8c76c9d4497602bc0d83
Zomba4ok <Antickz28@gmail.com>
2019-09-09 14:54:39 +0300

    Changed main function.

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class Car(db.Model):
     id = db.Column(db.BigInteger(), autoincrement=True, primary_key=True)
     car_brand = db.Column(db.String())
     body_type = db.Column(db.String())
-    owner = db.Column(None, db.ForeignKey('users.id'))
+    owner = db.Column(Integer, db.ForeignKey('users.id'))
 
 
 class User(db.Model):
@@ -39,20 +39,39 @@ class Test(db.Model):
     name = db.Column(db.String())
 
 
-async def query():
+async def get_cars_list_with_delay(delay):
+    await asyncio.sleep(delay)
     engine = await gino.create_engine('postgres://postgres:admin@localhost/postgres')
     async with engine.acquire() as conn:
-        objects = await conn.all('SELECT * FROM cars')
-    return objects
+        cars = await conn.all('SELECT * FROM cars')
+    return cars
+
+
+async def get_car_owner_with_delay(delay, car):
+    await asyncio.sleep(delay)
+    query = db.select([User]).where(User.id == car.owner).execution_options(loader=User)
+    owner = await query.gino.first()
+    return owner
 
 
 @app.route('/')
 async def get_car(request):
+    engine = await gino.create_engine('postgres://postgres:admin@localhost/postgres')
+    async with engine.acquire() as conn:
+        car = await conn.first('SELECT * FROM cars where car_brand = \'ferrari\'')
+
     start_time = time.time()
-    tasks = [query() for i in range(1, 5)]
-    await asyncio.wait(tasks)
+    tasks = [get_cars_list_with_delay(3), get_car_owner_with_delay(2, car)]
+    done, pending = await asyncio.wait(tasks)
     end_time = time.time()
-    return html(end_time - start_time)
+
+    time_delta = end_time - start_time
+    for item in done:
+        if not type(item.result()) is list:
+            owner = item.result()
+    output = 'Car owner is {}. Query duration is {}'.format(owner.first_name + ' ' + owner.last_name, time_delta)
+    return html(output)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9009)
